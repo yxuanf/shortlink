785403a4ef13d7c7e44c8fd024896a8fcff044a2
JustMay <yongxuanf@outlook.com>
2024-06-01 20:33:19 +0800

    feature: 短链接输入、输出类生成

---
+++ admin/src/main/java/org/yxuanf/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,37 @@
+package org.yxuanf.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.yxuanf.shortlink.admin.common.convention.result.Result;
+import org.yxuanf.shortlink.admin.remote.ShortLinkRemoteService;
+import org.yxuanf.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import org.yxuanf.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import org.yxuanf.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import org.yxuanf.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+
+@RestController
+
+public class ShortLinkController {
+    @Resource
+    private ShortLinkRemoteService shortLinkRemoteService;
+
+    /**
+     * 创建短链接
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return shortLinkRemoteService.createShortLink(requestParam);
+    }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return shortLinkRemoteService.pageShortLink(requestParam);
+    }
+}
---
+++ project/src/main/java/org/yxuanf/shortlink/project/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,14 @@
+package org.yxuanf.shortlink.project.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+import org.yxuanf.shortlink.project.dao.entity.ShortLinkDO;
+
+@Data
+public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
