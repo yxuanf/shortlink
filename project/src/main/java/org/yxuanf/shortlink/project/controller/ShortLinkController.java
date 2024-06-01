4e4330dda1df3e098bbd6b0b3c438cc3a6dd76aa
JustMay <yongxuanf@outlook.com>
2024-06-01 20:35:04 +0800

    feature: 短链接管理控制层

--- project/src/main/java/org/yxuanf/shortlink/project/controller/ShortLinkController.java
+++ project/src/main/java/org/yxuanf/shortlink/project/controller/ShortLinkController.java
@@ -1,14 +1,18 @@
 package org.yxuanf.shortlink.project.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.yxuanf.shortlink.project.common.convention.result.Result;
 import org.yxuanf.shortlink.project.common.convention.result.Results;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkCreateReqDTO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkPageReqDTO;
 import org.yxuanf.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
+import org.yxuanf.shortlink.project.dto.resp.ShortLinkGroupCountRespDTO;
+import org.yxuanf.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import org.yxuanf.shortlink.project.service.ShortLinkService;
+
+import java.util.List;
 
 /**
  * 短链接管理控制层
@@ -19,8 +23,29 @@ public class ShortLinkController {
 
     private final ShortLinkService shortLinkService;
 
+    /**
+     * 批量创建短链接
+     */
     @PostMapping("/api/short-link/v1/create")
     public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
         return Results.success(shortLinkService.createShortLink(requestParam));
     }
+
+    /**
+     * 分页查询短链接
+     */
+    @GetMapping("/api/short-link/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return Results.success(shortLinkService.pageShortLink(requestParam));
+    }
+
+    /**
+     * 查询短链接分组内数量
+     */
+    @GetMapping("/api/short-link/v1/count")
+    public Result<List<ShortLinkGroupCountRespDTO>> listGroupShortLinkCount(@RequestParam("requestParam") List<String> requestParam) {
+        return Results.success(shortLinkService.listGroupShortLinkCount(requestParam));
+    }
+
 }
+
