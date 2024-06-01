c580c95e64f0b4263932d7322636c35f69f21747
JustMay <yongxuanf@outlook.com>
2024-06-01 20:36:54 +0800

    feature: MybatisPlus分页配置

---
+++ project/src/main/java/org/yxuanf/shortlink/project/config/MybatisPlusConfig.java
@@ -0,0 +1,20 @@
+package org.yxuanf.shortlink.project.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MybatisPlus 分页插件
+ */
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
