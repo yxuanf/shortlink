c83d2dd7070dee2c3352c6ab9ed4b3115a1ba601
JustMay <yongxuanf@outlook.com>
2024-06-01 20:34:46 +0800

    feature: 在过滤器中返回异常信息，配置不拦截路径

--- admin/src/main/java/org/yxuanf/shortlink/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/yxuanf/shortlink/admin/common/biz/user/UserTransmitFilter.java
@@ -17,17 +17,27 @@
 
 package org.yxuanf.shortlink.admin.common.biz.user;
 
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.yxuanf.shortlink.admin.common.convention.exception.ClientException;
+import org.yxuanf.shortlink.admin.common.convention.result.Results;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.yxuanf.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
+import static org.yxuanf.shortlink.admin.common.enums.UserErrorCodeEnum.USER_TOKEN_FAIL;
 
 /**
  * 用户信息传输过滤器
@@ -35,20 +45,35 @@ import static org.yxuanf.shortlink.admin.common.constant.RedisCacheConstant.USER
 @RequiredArgsConstructor
 public class UserTransmitFilter implements Filter {
     private final StringRedisTemplate stringRedisTemplate;
+    private final HashSet<String> hashSet = new HashSet<>(Set.of("/api/short-link/admin/v1/user/login", "/api/short-link/v1/user/has-username"));
 
     @Override
     @SneakyThrows
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String path = httpServletRequest.getRequestURI();
+        String method = httpServletRequest.getMethod();
+        if ((hashSet.contains(path)) || (method.equalsIgnoreCase("POST") && path.matches("/api/short-link/admin/v1/user"))) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
         // 获取username 以及 token
         String username = httpServletRequest.getHeader("username");
         String token = httpServletRequest.getHeader("token");
+        // 若无法获取username 或 token 报错
+        if (!StrUtil.isAllNotBlank(username, token)) {
+            returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+            return;
+        }
         // 获取用户信息
         Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(USER_LOGIN_KEY + username, token);
         if (userInfoJsonStr != null) {
             // 将 用户信息放在用户上下文中
             UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
             UserContext.setUser(userInfoDTO);
+        } else {
+            returnJson((HttpServletResponse) servletResponse, JSON.toJSONString(Results.failure(new ClientException(USER_TOKEN_FAIL))));
+            return;
         }
         try {
             filterChain.doFilter(servletRequest, servletResponse);
@@ -56,6 +81,16 @@ public class UserTransmitFilter implements Filter {
             UserContext.removeUser();
         }
     }
+
+    private void returnJson(HttpServletResponse response, String json) throws Exception {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=utf-8");
+        try (PrintWriter writer = response.getWriter()) {
+            writer.print(json);
+        } catch (IOException ignored) {
+        }
+    }
+
 
 //    @SneakyThrows
 //    @Override
