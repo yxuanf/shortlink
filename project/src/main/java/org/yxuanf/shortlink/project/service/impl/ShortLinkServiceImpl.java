f6efd8a4f90b55f297b72b2e75c8ad0943cec478
JustMay <yongxuanf@outlook.com>
2024-06-03 13:24:45 +0800

    feature: 解决短链接跳转的缓存击穿问题

--- project/src/main/java/org/yxuanf/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/org/yxuanf/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -1,28 +1,48 @@
 package org.yxuanf.shortlink.project.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.dao.DuplicateKeyException;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yxuanf.shortlink.project.common.convention.exception.ClientException;
 import org.yxuanf.shortlink.project.common.convention.exception.ServiceException;
+import org.yxuanf.shortlink.project.common.enums.VailDateTypeEnum;
 import org.yxuanf.shortlink.project.dao.entity.ShortLinkDO;
+import org.yxuanf.shortlink.project.dao.entity.ShortLinkGotoDO;
+import org.yxuanf.shortlink.project.dao.mapper.ShortLinkGotoMapper;
 import org.yxuanf.shortlink.project.dao.mapper.ShortLinkMapper;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkCreateReqDTO;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkPageReqDTO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkUpdateReqDTO;
 import org.yxuanf.shortlink.project.dto.resp.ShortLinkCreateRespDTO;
 import org.yxuanf.shortlink.project.dto.resp.ShortLinkGroupCountRespDTO;
 import org.yxuanf.shortlink.project.dto.resp.ShortLinkPageRespDTO;
 import org.yxuanf.shortlink.project.service.ShortLinkService;
 import org.yxuanf.shortlink.project.toolkit.HashUtil;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import static org.yxuanf.shortlink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
+import static org.yxuanf.shortlink.project.common.constant.RedisKeyConstant.LOCK_GOTO_SHORT_LINK_KEY;
 
 @Slf4j
 @Service
@@ -31,6 +51,9 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
 
     private final RBloomFilter<String> shortUriCreateCachePenetrationBloomFilter;
     private final ShortLinkMapper shortLinkMapper;
+    private final ShortLinkGotoMapper shortLinkGotoMapper;
+    private final StringRedisTemplate stringRedisTemplate;
+    private final RedissonClient redissonClient;
 
     @Override
     public ShortLinkCreateRespDTO createShortLink(ShortLinkCreateReqDTO requestParam) {
@@ -50,13 +73,17 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .enableStatus(0)
                 .fullShortUrl(fullShortUrl)
                 .build();
+        // 创建短链接
+        ShortLinkGotoDO linkGotoDO = ShortLinkGotoDO.builder()
+                .fullShortUrl(fullShortUrl)
+                .gid(requestParam.getGid())
+                .build();
         try {
             // 作为兜底策略
-            /*
-             * 短链接入库成功，但是并没有添加到布隆过滤器中
-             * 实际上入库，但布隆过滤器显示短链不存在，此时再次插入该短链不就越过布隆过滤器，然后被唯一索引给拦截了
-             */
+            // 存在短链接入库成功，但Redis宕机的情况。此时通过唯一索引拦截
             baseMapper.insert(shortLinkDO);
+            // 插入路由表，通过完整短链接找到gid从而获取完整链接
+            shortLinkGotoMapper.insert(linkGotoDO);
             // 唯一索引冲突，查询数据库是否存在
         } catch (DuplicateKeyException ex) {
             // todo 误判短链接如何处理?
@@ -74,7 +101,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
         // 将完整短链接加入到布隆过滤器
         shortUriCreateCachePenetrationBloomFilter.add(fullShortUrl);
         return ShortLinkCreateRespDTO.builder()
-                .fullShortUrl(shortLinkDO.getFullShortUrl())
+                .fullShortUrl("http://" + shortLinkDO.getFullShortUrl())
                 .originUrl(shortLinkDO.getOriginUrl())
                 .gid(shortLinkDO.getGid())
                 .build();
@@ -101,6 +128,97 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
                 .groupBy("gid");
         List<Map<String, Object>> result = baseMapper.selectMaps(qw);
         return BeanUtil.copyToList(result, ShortLinkGroupCountRespDTO.class);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void updateShortLink(ShortLinkUpdateReqDTO requestParam) {
+        // 判断短链接分组是否被修改
+        LambdaQueryWrapper<ShortLinkDO> lqw = new LambdaQueryWrapper<>();
+        lqw.eq(ShortLinkDO::getFullShortUrl, requestParam.getFullShortUrl())
+                .eq(ShortLinkDO::getGid, requestParam.getGid())
+                .eq(ShortLinkDO::getDelFlag, 0)
+                .eq(ShortLinkDO::getEnableStatus, 0);
+        ShortLinkDO hasShortLinkDO = baseMapper.selectOne(lqw);
+        if (hasShortLinkDO == null) {
+            throw new ClientException("短链接记录不存在");
+        }
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .domain(hasShortLinkDO.getDomain())
+                .shortUri(hasShortLinkDO.getShortUri())
+                .favicon(hasShortLinkDO.getFavicon())
+                .createdType(hasShortLinkDO.getCreatedType())
+                .gid(requestParam.getGid())
+                .originUrl(requestParam.getOriginUrl())
+                .describe(requestParam.getDescribe())
+                .validDateType(requestParam.getValidDateType())
+                .validDate(requestParam.getValidDate())
+                .build();
+        LambdaUpdateWrapper<ShortLinkDO> luw = new LambdaUpdateWrapper<>();
+        // 短链接分组未被修改
+        if (Objects.equals(hasShortLinkDO.getGid(), requestParam.getGid())) {
+            luw.eq(ShortLinkDO::getFullShortUrl, requestParam.getFullShortUrl())
+                    .eq(ShortLinkDO::getGid, requestParam.getGid())
+                    .eq(ShortLinkDO::getDelFlag, 0)
+                    .eq(ShortLinkDO::getEnableStatus, 0)
+                    .set(Objects.equals(requestParam.getValidDateType(), VailDateTypeEnum.PERMANENT.getType()), ShortLinkDO::getValidDate, null);
+            baseMapper.update(shortLinkDO, luw);
+        }
+        // 若分组被修改，则先删除原先的分组，在新添更新后的
+        else {
+            luw.eq(ShortLinkDO::getFullShortUrl, requestParam.getFullShortUrl())
+                    .eq(ShortLinkDO::getGid, hasShortLinkDO.getGid())
+                    .eq(ShortLinkDO::getDelFlag, 0)
+                    .eq(ShortLinkDO::getEnableStatus, 0);
+            baseMapper.delete(luw);
+            baseMapper.insert(shortLinkDO);
+        }
+    }
+
+    // 短链接重定向
+    @Override
+    public void restoreUrl(String shortUri, ServletRequest request, ServletResponse response) throws IOException {
+        // 返回主机名（nurl.ink）
+        String serverName = request.getServerName();
+        String fullShortUrl = serverName + "/" + shortUri;
+        // 首先尝试从缓存中获取原始链接
+        String originalLink = stringRedisTemplate.opsForValue().get(String.format(GOTO_SHORT_LINK_KEY, fullShortUrl));
+        if (StrUtil.isNotBlank(originalLink)) {
+            ((HttpServletResponse) response).sendRedirect(originalLink);
+            return;
+        }
+        RLock lock = redissonClient.getLock(String.format(LOCK_GOTO_SHORT_LINK_KEY, fullShortUrl));
+        lock.lock();
+        try {
+            originalLink = stringRedisTemplate.opsForValue().get(String.format(GOTO_SHORT_LINK_KEY, fullShortUrl));
+            // 双重判定锁，只有第一个拿到锁的请求进行缓存重构，之后拿到锁的请求直接查询缓存即可
+            if (StrUtil.isNotBlank(originalLink)) {
+                ((HttpServletResponse) response).sendRedirect(originalLink);
+                return;
+            }
+            // 通过路由表获取短链接的gid
+            LambdaQueryWrapper<ShortLinkGotoDO> link_Goto_lqw = Wrappers.lambdaQuery(ShortLinkGotoDO.class)
+                    .eq(ShortLinkGotoDO::getFullShortUrl, fullShortUrl);
+            ShortLinkGotoDO linkGotoDO = shortLinkGotoMapper.selectOne(link_Goto_lqw);
+            if (linkGotoDO == null) {
+                //todo 需要风控
+                return;
+            }
+            LambdaQueryWrapper<ShortLinkDO> lqw = Wrappers.lambdaQuery(ShortLinkDO.class)
+                    .eq(ShortLinkDO::getFullShortUrl, fullShortUrl)
+                    .eq(ShortLinkDO::getGid, linkGotoDO.getGid())
+                    .eq(ShortLinkDO::getDelFlag, 0)
+                    .eq(ShortLinkDO::getEnableStatus, 0);
+            ShortLinkDO shortLinkDO = baseMapper.selectOne(lqw);
+            // 重定向原始链接（重定向URL）
+            if (shortLinkDO != null) {
+                // 第一个获取锁的进程才能添加
+                stringRedisTemplate.opsForValue().set(String.format(GOTO_SHORT_LINK_KEY, fullShortUrl), shortLinkDO.getOriginUrl());
+                ((HttpServletResponse) response).sendRedirect(shortLinkDO.getOriginUrl());
+            }
+        } finally {
+            lock.unlock();
+        }
     }
 
     private String generateSuffix(ShortLinkCreateReqDTO requestParam) {
