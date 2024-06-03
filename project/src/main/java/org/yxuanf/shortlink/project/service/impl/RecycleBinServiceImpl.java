0d0769bcf997ec7dc41308cae579a8865d6235a7
JustMay <yongxuanf@outlook.com>
2024-06-03 21:46:12 +0800

    feature: 短链接回收功能

---
+++ project/src/main/java/org/yxuanf/shortlink/project/service/impl/RecycleBinServiceImpl.java
@@ -0,0 +1,71 @@
+package org.yxuanf.shortlink.project.service.impl;
+
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+import org.yxuanf.shortlink.project.dao.entity.ShortLinkDO;
+import org.yxuanf.shortlink.project.dao.mapper.ShortLinkMapper;
+import org.yxuanf.shortlink.project.dto.req.RecycleBinRecoverReqDTO;
+import org.yxuanf.shortlink.project.dto.req.RecycleBinRemoveReqDTO;
+import org.yxuanf.shortlink.project.dto.req.RecycleBinSaveReqDTO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkRecycleBinPageReqDTO;
+import org.yxuanf.shortlink.project.dto.resp.ShortLinkPageRespDTO;
+import org.yxuanf.shortlink.project.service.RecycleBinService;
+
+import static org.yxuanf.shortlink.project.common.constant.RedisKeyConstant.GOTO_SHORT_LINK_KEY;
+
+/**
+ * 回收站管理接口实现层
+ */
+@Service
+@RequiredArgsConstructor
+public class RecycleBinServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLinkDO> implements RecycleBinService {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public void saveRecycleBin(RecycleBinSaveReqDTO requestParam) {
+        LambdaUpdateWrapper<ShortLinkDO> updateWrapper = Wrappers.lambdaUpdate(ShortLinkDO.class)
+                .eq(ShortLinkDO::getFullShortUrl, requestParam.getFullShortUrl())
+                .eq(ShortLinkDO::getGid, requestParam.getGid())
+                .eq(ShortLinkDO::getEnableStatus, 0)
+                .eq(ShortLinkDO::getDelFlag, 0);
+        ShortLinkDO shortLinkDO = ShortLinkDO.builder()
+                .enableStatus(1)
+                .build();
+        baseMapper.update(shortLinkDO, updateWrapper);
+        // 移除缓存
+        stringRedisTemplate.delete(String.format(GOTO_SHORT_LINK_KEY, requestParam.getFullShortUrl()));
+    }
+
+    @Override
+    public IPage<ShortLinkPageRespDTO> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        LambdaQueryWrapper<ShortLinkDO> lqw = Wrappers.lambdaQuery(ShortLinkDO.class)
+                .in(ShortLinkDO::getGid, requestParam.getGidList())
+                .eq(ShortLinkDO::getEnableStatus, 1)
+                .eq(ShortLinkDO::getDelFlag, 0)
+                .orderByDesc(ShortLinkDO::getUpdateTime);
+        IPage<ShortLinkDO> result = baseMapper.selectPage(requestParam, lqw);
+        return result.convert(each -> {
+            ShortLinkPageRespDTO bean = BeanUtil.toBean(each, ShortLinkPageRespDTO.class);
+            bean.setDomain("http://" + bean.getDomain());
+            return bean;
+        });
+    }
+
+    @Override
+    public void recoverRecycleBin(RecycleBinRecoverReqDTO requestParam) {
+
+    }
+
+    @Override
+    public void removeRecycleBin(RecycleBinRemoveReqDTO requestParam) {
+
+    }
+}
