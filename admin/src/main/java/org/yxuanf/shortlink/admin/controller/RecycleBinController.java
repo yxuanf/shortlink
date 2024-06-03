a9f1f4849102c9164e99602efb44b23861d38c26
JustMay <yongxuanf@outlook.com>
2024-06-03 21:44:22 +0800

    feature: 获取原始链接的标题

---
+++ admin/src/main/java/org/yxuanf/shortlink/admin/controller/RecycleBinController.java
@@ -0,0 +1,60 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yxuanf.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.yxuanf.shortlink.admin.common.convention.result.Result;
+import org.yxuanf.shortlink.admin.common.convention.result.Results;
+import org.yxuanf.shortlink.admin.remote.ShortLinkRemoteService;
+import org.yxuanf.shortlink.admin.remote.dto.req.RecycleBinSaveReqDTO;
+import org.yxuanf.shortlink.admin.remote.dto.req.ShortLinkRecycleBinPageReqDTO;
+import org.yxuanf.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.yxuanf.shortlink.admin.service.RecycleBinService;
+
+/**
+ * 回收站管理控制层
+ */
+@RestController(value = "recycleBinControllerByAdmin")
+@RequiredArgsConstructor
+public class RecycleBinController {
+
+    private final ShortLinkRemoteService shortLinkRemoteService;
+    private final RecycleBinService recycleBinService;
+
+    /**
+     * 保存回收站
+     */
+    @PostMapping("/api/short-link/admin/v1/recycle-bin/save")
+    public Result<Void> saveRecycleBin(@RequestBody RecycleBinSaveReqDTO requestParam) {
+        shortLinkRemoteService.saveRecycleBin(requestParam);
+        return Results.success();
+    }
+
+    /**
+     * 分页查询回收站短链接
+     */
+    @GetMapping("/api/short-link/admin/v1/recycle-bin/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkRecycleBinPageReqDTO requestParam) {
+        return recycleBinService.pageRecycleBinShortLink(requestParam);
+    }
+}
