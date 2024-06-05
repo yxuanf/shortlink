db1221512c3a15766d8714db646718489fc07679
JustMay <yongxuanf@outlook.com>
2024-06-05 17:26:30 +0800

    feature: 新增修改时间

--- admin/src/main/java/org/yxuanf/shortlink/admin/remote/dto/resp/ShortLinkPageRespDTO.java
+++ admin/src/main/java/org/yxuanf/shortlink/admin/remote/dto/resp/ShortLinkPageRespDTO.java
@@ -77,6 +77,11 @@ public class ShortLinkPageRespDTO {
     private Date createTime;
 
     /**
+     * 修改时间
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date updateTime;
+    /**
      * 描述
      */
     private String describe;
