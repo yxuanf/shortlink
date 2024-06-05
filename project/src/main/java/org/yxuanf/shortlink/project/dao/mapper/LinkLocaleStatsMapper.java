bd7eef0149c7ef7bb852d324beff4c2b95f4303a
JustMay <yongxuanf@outlook.com>
2024-06-05 17:21:17 +0800

    feature: 新增获取指定日期内地区监控数据

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkLocaleStatsMapper.java
@@ -59,21 +59,4 @@ public interface LinkLocaleStatsMapper extends BaseMapper<LinkLocaleStatsDO> {
             "    tlls.full_short_url, tl.gid, tlls.province;")
     List<LinkLocaleStatsDO> listLocaleByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
 
-//    /**
-//     * 根据分组获取指定日期内地区监控数据
-//     */
-//    @Select("SELECT " +
-//            "    tlls.province, " +
-//            "    SUM(tlls.cnt) AS cnt " +
-//            "FROM " +
-//            "    t_link tl INNER JOIN " +
-//            "    t_link_locale_stats tlls ON tl.full_short_url = tlls.full_short_url " +
-//            "WHERE " +
-//            "    tl.gid = #{param.gid} " +
-//            "    AND tl.del_flag = '0' " +
-//            "    AND tl.enable_status = '0' " +
-//            "    AND tlls.date BETWEEN #{param.startDate} and #{param.endDate} " +
-//            "GROUP BY " +
-//            "    tl.gid, tlls.province;")
-//    List<LinkLocaleStatsDO> listLocaleByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
 }
