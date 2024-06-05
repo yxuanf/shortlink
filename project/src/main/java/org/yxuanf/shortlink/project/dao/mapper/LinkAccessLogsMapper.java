fc5f056593f3fe41df5e5160e48db5db83412215
JustMay <yongxuanf@outlook.com>
2024-06-05 17:19:40 +0800

    feature: 增加访问日志监控持久层

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkAccessLogsMapper.java
@@ -4,11 +4,15 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.yxuanf.shortlink.project.dao.entity.LinkAccessLogsDO;
+import org.yxuanf.shortlink.project.dao.entity.LinkAccessStatsDO;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 
 import java.util.HashMap;
 import java.util.List;
 
+/**
+ * 访问日志监控持久层
+ */
 public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
 
     /**
@@ -32,4 +36,49 @@ public interface LinkAccessLogsMapper extends BaseMapper<LinkAccessLogsDO> {
             "    count DESC " +
             "LIMIT 5;")
     List<HashMap<String, Object>> listTopIpByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据短链接获取指定日期内PV、UV、UIP数据
+     */
+    @Select("SELECT " +
+            "    COUNT(tlal.user) AS pv, " +
+            "    COUNT(DISTINCT tlal.user) AS uv, " +
+            "    COUNT(DISTINCT tlal.ip) AS uip " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_access_logs tlal ON tl.full_short_url = tlal.full_short_url " +
+            "WHERE " +
+            "    tlal.full_short_url = #{param.fullShortUrl} " +
+            "    AND tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = #{param.enableStatus} " +
+            "    AND tlal.create_time BETWEEN CONCAT(#{param.startDate},' 00:00:00') and CONCAT(#{param.endDate},' 23:59:59')" +
+            "GROUP BY " +
+            "    tlal.full_short_url, tl.gid;")
+    LinkAccessStatsDO findPvUvUidStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据短链接获取指定日期内新旧访客数据
+     */
+    @Select("SELECT " +
+            "    SUM(old_user) AS oldUserCnt, " +
+            "    SUM(new_user) AS newUserCnt " +
+            "FROM ( " +
+            "    SELECT " +
+            "        CASE WHEN COUNT(DISTINCT DATE(tlal.create_time)) > 1 THEN 1 ELSE 0 END AS old_user, " +
+            "        CASE WHEN COUNT(DISTINCT DATE(tlal.create_time)) = 1 AND MAX(tlal.create_time) >= #{param.startDate} AND MAX(tlal.create_time) <= #{param.endDate} THEN 1 ELSE 0 END AS new_user " +
+            "    FROM " +
+            "        t_link tl INNER JOIN " +
+            "        t_link_access_logs tlal " +
+            "    ON tl.full_short_url = tlal.full_short_url " +
+            "    WHERE " +
+            "        tlal.full_short_url = #{param.fullShortUrl} " +
+            "        AND tl.gid = #{param.gid} " +
+            "        AND tl.enable_status = #{param.enableStatus} " +
+            "        AND tl.del_flag = '0' " +
+            "    GROUP BY " +
+            "        tlal.user " +
+            ") AS user_counts;")
+    HashMap<String, Object> findUvTypeCntByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
 }
