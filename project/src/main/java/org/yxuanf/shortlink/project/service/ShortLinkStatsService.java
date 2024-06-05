29fc610b462c2dd1c9c05f8d7301c5c230e1930c
JustMay <yongxuanf@outlook.com>
2024-06-05 17:30:26 +0800

    feature:新增短链接监控接口层

---
+++ project/src/main/java/org/yxuanf/shortlink/project/service/ShortLinkStatsService.java
@@ -0,0 +1,18 @@
+package org.yxuanf.shortlink.project.service;
+
+import org.yxuanf.shortlink.project.dto.req.ShortLinkStatsReqDTO;
+import org.yxuanf.shortlink.project.dto.resp.ShortLinkStatsRespDTO;
+
+/**
+ * 短链接监控接口层
+ */
+public interface ShortLinkStatsService {
+
+    /**
+     * 获取单个短链接监控数据
+     *
+     * @param requestParam 获取短链接监控数据入参
+     * @return 短链接监控数据
+     */
+    ShortLinkStatsRespDTO oneShortLinkStats(ShortLinkStatsReqDTO requestParam);
+}
