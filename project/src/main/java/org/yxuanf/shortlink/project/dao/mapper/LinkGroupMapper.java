d371d8bf2a88b8644d83d0423f065b53be7903d7
JustMay <yongxuanf@outlook.com>
2024-06-05 17:20:58 +0800

    feature: 短链接分组持久层

---
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkGroupMapper.java
@@ -0,0 +1,10 @@
+package org.yxuanf.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.yxuanf.shortlink.project.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+ */
+public interface LinkGroupMapper extends BaseMapper<GroupDO> {
+}
