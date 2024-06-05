1c59869046bc698d22e10bd4413288f2c6780e47
JustMay <yongxuanf@outlook.com>
2024-06-05 17:25:54 +0800

    feature: 短链接分页返回参数

---
+++ admin/src/main/java/org/yxuanf/shortlink/admin/remote/dto/req/ShortLinkPageReqDTO.java
@@ -0,0 +1,17 @@
+package org.yxuanf.shortlink.admin.remote.dto.req;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.Data;
+import org.yxuanf.shortlink.admin.dao.entity.ShortLinkDO;
+
+/**
+ * 短链接分页返回参数
+ */
+@Data
+public class ShortLinkPageReqDTO extends Page<ShortLinkDO> {
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+}
