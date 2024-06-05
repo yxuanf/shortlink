84c99fa547f4ccc199e8b13d0e78c91c704acbdf
JustMay <yongxuanf@outlook.com>
2024-06-05 15:03:13 +0800

    feature: 记录访问设备监控数据问持久层

---
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -0,0 +1,18 @@
+package org.yxuanf.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.yxuanf.shortlink.project.dao.entity.LinkNetworkStatsDO;
+
+public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
+
+    /**
+     * 记录访问设备监控数据
+     */
+    @Insert("INSERT INTO " +
+            "t_link_network_stats (full_short_url, date, cnt, network, create_time, update_time, del_flag) " +
+            "VALUES( #{linkNetworkStats.fullShortUrl}, #{linkNetworkStats.date}, #{linkNetworkStats.cnt}, #{linkNetworkStats.network}, NOW(), NOW(), 0) " +
+            "ON DUPLICATE KEY UPDATE cnt = cnt +  #{linkNetworkStats.cnt};")
+    void shortLinkNetworkState(@Param("linkNetworkStats") LinkNetworkStatsDO linkNetworkStatsDO);
+}
