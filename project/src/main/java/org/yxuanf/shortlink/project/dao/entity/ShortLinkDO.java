74d6075842bd8b317ae6c242fe591e541d5d3003
JustMay <yongxuanf@outlook.com>
2024-06-07 01:33:51 +0800

    feature:新增统计数据

--- project/src/main/java/org/yxuanf/shortlink/project/dao/entity/ShortLinkDO.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/entity/ShortLinkDO.java
@@ -80,7 +80,38 @@ public class ShortLinkDO extends BaseDO {
      * 网站图标
      */
     private String favicon;
+    /**
+     * 历史PV
+     */
+    private Integer totalPv;
 
+    /**
+     * 历史UV
+     */
+    private Integer totalUv;
+
+    /**
+     * 历史UIP
+     */
+    private Integer totalUip;
+
+    /**
+     * 今日PV
+     */
+    @TableField(exist = false)
+    private Integer todayPv;
+
+    /**
+     * 今日UV
+     */
+    @TableField(exist = false)
+    private Integer todayUv;
+
+    /**
+     * 今日UIP
+     */
+    @TableField(exist = false)
+    private Integer todayUip;
     /**
      * 删除时间
      */
