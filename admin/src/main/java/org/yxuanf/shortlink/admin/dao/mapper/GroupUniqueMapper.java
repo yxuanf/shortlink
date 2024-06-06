5e97727a9bcd18e613a214e6410b2b316c2039e8
JustMay <yongxuanf@outlook.com>
2024-06-07 01:20:53 +0800

    feature: 短链接分组唯一路由持久层

---
+++ admin/src/main/java/org/yxuanf/shortlink/admin/dao/mapper/GroupUniqueMapper.java
@@ -0,0 +1,10 @@
+package org.yxuanf.shortlink.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.yxuanf.shortlink.admin.dao.entity.GroupUniqueDO;
+
+/**
+ * 短链接分组唯一路由持久层
+ */
+public interface GroupUniqueMapper extends BaseMapper<GroupUniqueDO> {
+}
