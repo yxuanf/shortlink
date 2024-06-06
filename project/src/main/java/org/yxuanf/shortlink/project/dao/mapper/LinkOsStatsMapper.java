5005780ffa83035feb9a32d687341cc635266688
JustMay <yongxuanf@outlook.com>
2024-06-07 01:35:21 +0800

    feature:根据分组获取指定日期内操作系统监控数据

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkOsStatsMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkOsStatsMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.yxuanf.shortlink.project.dao.entity.LinkOsStatsDO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkGroupStatsReqDTO;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 
 import java.util.HashMap;
@@ -27,7 +28,7 @@ public interface LinkOsStatsMapper extends BaseMapper<LinkOsStatsDO> {
      * 根据短链接获取指定日期内操作系统监控数据
      */
     @Select("SELECT " +
-            "    tlos.os, " +
+            "    tlos.os , " +
             "    SUM(tlos.cnt) AS count " +
             "FROM " +
             "    t_link tl INNER JOIN " +
@@ -41,4 +42,22 @@ public interface LinkOsStatsMapper extends BaseMapper<LinkOsStatsDO> {
             "GROUP BY " +
             "    tlos.full_short_url, tl.gid, tlos.os;")
     List<HashMap<String, Object>> listOsStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据分组获取指定日期内操作系统监控数据
+     */
+    @Select("SELECT " +
+            "    tlos.os, " +
+            "    SUM(tlos.cnt) AS count " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_os_stats tlos ON tl.full_short_url = tlos.full_short_url " +
+            "WHERE " +
+            "    tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = '0' " +
+            "    AND tlos.date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    tl.gid, tlos.os;")
+    List<HashMap<String, Object>> listOsStatsByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
 }
