c930ff1b0ad8f6e4dda55dc3bc394a95509b3ef7
JustMay <yongxuanf@outlook.com>
2024-06-07 01:25:10 +0800

    feature: 短链接访问统计自增、分页统计短链接

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -1,10 +1,27 @@
 package org.yxuanf.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import org.apache.ibatis.annotations.Param;
 import org.yxuanf.shortlink.project.dao.entity.ShortLinkDO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkPageReqDTO;
 
 /**
  * 短链接持久层
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * 短链接访问统计自增
+     */
+    void incrementStats(@Param("gid") String gid,
+                        @Param("fullShortUrl") String fullShortUrl,
+                        @Param("totalPv") Integer totalPv,
+                        @Param("totalUv") Integer totalUv,
+                        @Param("totalUip") Integer totalUip);
+
+    /**
+     * 分页统计短链接
+     */
+    IPage<ShortLinkDO> pageLink(ShortLinkPageReqDTO requestParam);
+
 }
