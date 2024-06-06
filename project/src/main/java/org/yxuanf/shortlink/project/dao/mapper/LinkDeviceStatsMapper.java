942a103ff4252ca323118b2ce8c8374738546528
JustMay <yongxuanf@outlook.com>
2024-06-07 01:24:40 +0800

    feature: Add根据分组获取指定日期内访问设备监控数据

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkDeviceStatsMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkDeviceStatsMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.yxuanf.shortlink.project.dao.entity.LinkDeviceStatsDO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkGroupStatsReqDTO;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 
 import java.util.List;
@@ -41,4 +42,22 @@ public interface LinkDeviceStatsMapper extends BaseMapper<LinkDeviceStatsDO> {
             "GROUP BY " +
             "    tlds.full_short_url, tl.gid, tlds.device;")
     List<LinkDeviceStatsDO> listDeviceStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据分组获取指定日期内访问设备监控数据
+     */
+    @Select("SELECT " +
+            "    tlds.device, " +
+            "    SUM(tlds.cnt) AS cnt " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_device_stats tlds ON tl.full_short_url = tlds.full_short_url " +
+            "WHERE " +
+            "    tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = '0' " +
+            "    AND tlds.date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    tl.gid, tlds.device;")
+    List<LinkDeviceStatsDO> listDeviceStatsByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
 }
