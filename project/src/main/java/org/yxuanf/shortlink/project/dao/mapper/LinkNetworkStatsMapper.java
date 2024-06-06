768c7e13edc5b7d3103d96d6a390cf3e190b4522
JustMay <yongxuanf@outlook.com>
2024-06-07 01:34:34 +0800

    feature:add 根据分组获取指定日期内访问网络监控数据

--- project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkNetworkStatsMapper.java
+++ project/src/main/java/org/yxuanf/shortlink/project/dao/mapper/LinkNetworkStatsMapper.java
@@ -5,6 +5,7 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.yxuanf.shortlink.project.dao.entity.LinkNetworkStatsDO;
+import org.yxuanf.shortlink.project.dto.req.ShortLinkGroupStatsReqDTO;
 import org.yxuanf.shortlink.project.dto.req.ShortLinkStatsReqDTO;
 
 import java.util.List;
@@ -41,4 +42,22 @@ public interface LinkNetworkStatsMapper extends BaseMapper<LinkNetworkStatsDO> {
             "GROUP BY " +
             "    tlns.full_short_url, tl.gid, tlns.network;")
     List<LinkNetworkStatsDO> listNetworkStatsByShortLink(@Param("param") ShortLinkStatsReqDTO requestParam);
+
+    /**
+     * 根据分组获取指定日期内访问网络监控数据
+     */
+    @Select("SELECT " +
+            "    tlns.network, " +
+            "    SUM(tlns.cnt) AS cnt " +
+            "FROM " +
+            "    t_link tl INNER JOIN " +
+            "    t_link_network_stats tlns ON tl.full_short_url = tlns.full_short_url " +
+            "WHERE " +
+            "    tl.gid = #{param.gid} " +
+            "    AND tl.del_flag = '0' " +
+            "    AND tl.enable_status = '0' " +
+            "    AND tlns.date BETWEEN #{param.startDate} and #{param.endDate} " +
+            "GROUP BY " +
+            "    tl.gid, tlns.network;")
+    List<LinkNetworkStatsDO> listNetworkStatsByGroup(@Param("param") ShortLinkGroupStatsReqDTO requestParam);
 }
