276419ee66d542e18392ce80b4f0610ca7b04679
JustMay <yongxuanf@outlook.com>
2024-06-13 13:00:45 +0800

    feature: Redis Stream 消息队列配置

---
+++ project/src/main/java/org/yxuanf/shortlink/project/config/RedisStreamConfiguration.java
@@ -0,0 +1,99 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.yxuanf.shortlink.project.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.stream.Consumer;
+import org.springframework.data.redis.connection.stream.MapRecord;
+import org.springframework.data.redis.connection.stream.ReadOffset;
+import org.springframework.data.redis.connection.stream.StreamOffset;
+import org.springframework.data.redis.stream.StreamMessageListenerContainer;
+import org.yxuanf.shortlink.project.mq.consumer.ShortLinkStatsSaveConsumer;
+
+import java.time.Duration;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import static org.yxuanf.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_GROUP_KEY;
+import static org.yxuanf.shortlink.project.common.constant.RedisKeyConstant.SHORT_LINK_STATS_STREAM_TOPIC_KEY;
+
+
+/**
+ * Redis Stream 消息队列配置
+ */
+@Configuration
+@RequiredArgsConstructor
+public class RedisStreamConfiguration {
+
+    // redis连接工厂
+    private final RedisConnectionFactory redisConnectionFactory;
+    // 消费者
+    private final ShortLinkStatsSaveConsumer shortLinkStatsSaveConsumer;
+
+    /**
+     * 消费者通过线程池执行
+     * @return 返回自定义线程池
+     */
+    @Bean
+    public ExecutorService asyncStreamConsumer() {
+        AtomicInteger index = new AtomicInteger();
+        // 获取cpu核数（16）
+        int processors = Runtime.getRuntime().availableProcessors();
+        return new ThreadPoolExecutor(processors,
+                processors + processors >> 1,
+                60,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(),
+                runnable -> {
+                    Thread thread = new Thread(runnable);
+                    thread.setName("stream_consumer_short-link_stats_" + index.incrementAndGet());
+                    thread.setDaemon(true);
+                    return thread;
+                }
+        );
+    }
+
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer(ExecutorService asyncStreamConsumer) {
+        // 监听绑定
+        StreamMessageListenerContainer.StreamMessageListenerContainerOptions<String, MapRecord<String, String, String>> options =
+                StreamMessageListenerContainer.StreamMessageListenerContainerOptions
+                        .builder()
+                        // 一次最多获取多少条消息
+                        .batchSize(10)
+                        // 执行从 Stream 拉取到消息的任务流程
+                        .executor(asyncStreamConsumer)
+                        // 如果没有拉取到消息，需要阻塞的时间。不能大于 ${spring.data.redis.timeout}，否则会超时
+                        .pollTimeout(Duration.ofSeconds(3))
+                        .build();
+
+        StreamMessageListenerContainer<String, MapRecord<String, String, String>> streamMessageListenerContainer =
+                StreamMessageListenerContainer.create(redisConnectionFactory, options);
+        // 绑定监听
+        streamMessageListenerContainer.receiveAutoAck(Consumer.from(SHORT_LINK_STATS_STREAM_GROUP_KEY, "stats-consumer"),
+                StreamOffset.create(SHORT_LINK_STATS_STREAM_TOPIC_KEY, ReadOffset.lastConsumed()), shortLinkStatsSaveConsumer);
+        return streamMessageListenerContainer;
+    }
+}
