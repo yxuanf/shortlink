b309e149d1e64751cede6f6ddde7012759ac4633
JustMay <yongxuanf@outlook.com>
2024-06-13 12:56:23 +0800

    feature: 短链接统计实体

---
+++ project/src/main/java/org/yxuanf/shortlink/project/dto/biz/ShortLinkStatsRecordDTO.java
@@ -0,0 +1,78 @@
+package org.yxuanf.shortlink.project.dto.biz;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 短链接统计实体
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShortLinkStatsRecordDTO {
+
+    /**
+     * 完整短链接
+     */
+    private String fullShortUrl;
+
+    /**
+     * 访问用户IP
+     */
+    private String remoteAddr;
+
+    /**
+     * 操作系统
+     */
+    private String os;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 浏览器
+     */
+    private String browser;
+
+    /**
+     * 操作设备
+     */
+    private String device;
+
+    /**
+     * 网络
+     */
+    private String network;
+
+    /**
+     * UV
+     */
+    private String uv;
+
+    /**
+     * UV访问标识
+     */
+    private Boolean uvFirstFlag;
+
+    /**
+     * UIP访问标识
+     */
+    private Boolean uipFirstFlag;
+
+    /**
+     * 消息队列唯一标识
+     */
+    private String keys;
+
+    /**
+     * 当前时间
+     */
+    private Date currentDate;
+}
